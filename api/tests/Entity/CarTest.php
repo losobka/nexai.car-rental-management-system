a00b2aa16931c470346fa2e16994f837ca0671f9
losobka <lukasz.osobka@gmail.com>
2025-01-19 15:55:11 +0100

    @API - stworzenie testów E2E na POST /cars (App\Tests\Entity\CarTest): - próba dodania poprawnego samochodu - próba dodania niepoprawnego samochodu

--- api/tests/Entity/CarTest.php
+++ api/tests/Entity/CarTest.php
@@ -46,10 +46,99 @@ class CarTest extends ApiTestCase
 
         CarFactory::truncate();
     }
+
+    /**
+     * @dataProvider validCarProvider
+     */
+    public function testShouldCreateACar(int $expectedId, string $brand, string $registrationNumber, string $vin, bool $rented, string | null $customerEmail, string | null $customerAddress): void
+    {
+        // given
+        $car = (object) [
+            'brand' => 'BMW',
+            'registrationNumber' => 'TEST123',
+            'vin' => 'DFMDU34X8MUD84229',
+            'rented' => false,
+            'customerEmail' => $customerEmail,
+            'customerAddress' => $customerAddress
+        ];
+        $httpClient = static::createClient(defaultOptions: ['headers'=> ['Accept' => 'application/json']]);
+        $serializer = self::getContainer()->get(SerializerInterface::class);
+
+        // when
+        $response = $httpClient->request('POST', '/cars', ['json' => $car]);
+
+        // then
+        $deserializedResponse = $response->toArray(false);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+        $this->assertResponseHeaderSame('Content-Type','application/json; charset=utf-8');
+        $this->assertMatchesResourceItemJsonSchema(Car::class);
+        $this->assertSame(
+            $serializer->normalize(
+                array_merge(
+                    ['id' => $expectedId],
+                    (array) $car,
+                    ['latitude' => 0.0, 'longitude' => 0.0]
+                ),
+                'json',
+                [AbstractObjectNormalizer::SKIP_NULL_VALUES => true]
+            ),
+            $serializer->normalize($deserializedResponse, 'json', [AbstractObjectNormalizer::SKIP_NULL_VALUES => true])
+        );
+
+        CarFactory::truncate();
+    }
+
+    /**
+     * @dataProvider invalidCarProvider
+     */
+    public function testShouldNotCreateACar(int $expectedId, string $brand, string $registrationNumber, string $vin, bool $rented, string | null $customerEmail, string | null $customerAddress, string $expectedMessage): void
+    {
+        // given
+        $car = (object) [
+            'brand' => $brand,
+            'registrationNumber' => $registrationNumber,
+            'vin' => $vin,
+            'rented' => $rented,
+            'customerEmail' => $customerEmail,
+            'customerAddress' => $customerAddress
+        ];
+        $httpClient = static::createClient(defaultOptions: ['headers'=> ['Accept' => 'application/json']]);
+
+        // when
+        $response = $httpClient->request('POST', '/cars', ['json' => $car]);
+
+        // then
+        $deserializedResponse = $response->toArray(false);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertResponseHeaderSame('Content-Type','application/problem+json; charset=utf-8');
+        $this->assertCount(1, $deserializedResponse['violations']);
+        $this->assertSame($expectedMessage, $deserializedResponse['violations'][0]['message']);
+
+        CarFactory::truncate();
+    }
     private function countOfCarsInCollectionProvider(): iterable
     {
         yield 'empty' => [0];
-        yield 'with 20' => [20];
-        yield 'with 1000' => [1000];
+        yield '20' => [20];
+    }
+
+    private function validCarProvider(): iterable
+    {
+        yield 'not rented' => [1, 'BMW', 'TEST123', 'DFMDU34X8MUD84229', false, null, null];
+        yield 'rented' => [2, 'Audi', 'WA4567', 'YV1RS61T532259048', true, 'imie.nazwisko@example.nextai', 'ul. Sezamkowa, 11-222 Warszawa'];
+    }
+
+    private function invalidCarProvider(): iterable
+    {
+        yield 'with incorrect brand' => [1, 'incorrect-brand', 'TEST123', 'DFMDU34X8MUABC229', false, null, null, 'The value you selected is not a valid choice.'];
+        yield 'with too short registration number' => [2, 'Audi', 'T', 'KMHCM36C05U123456', false, null, null, 'Invalid registration number'];
+        yield 'with too long registration number' => [3, 'BMW', 'T123456789ABC', 'DFMDU34X8MUD84229', false, null, null, 'Invalid registration number'];
+        yield 'with invalid registration number (number as first character)' => [4, 'Toyota', '1INVALID', 'DFMDU34X8MUD84229', false, null, null, 'Invalid registration number'];
+        yield 'with invalid vin (number as first character)' => [5, 'Honda', '1INVALID', '9FMDU34X8MUD8422D', false, null, null, 'Invalid registration number'];
+        yield 'rented with invalid customer email' => [6, 'Tesla', 'KR1234AB', 'HGCM82633A1234567', true, 'invalid_email', 'ul. Sezamkowa 5, 11-222 WarszAwa', 'This value is not a valid email address.'];
+        yield 'not rented with customer email' => [7, 'Ferrari', 'WA5678CD', 'WDBUF56J76A123456', false, 'imie.nazwisko@example.nextai', null, 'Customer email address should be empty'];
+        yield 'not rented with customer address' => [8, 'Volvo', 'PO4321EF', 'WBA3A5C59FF123456', false, null, 'ul. Sezamkowa, 11-222 Warszawa', 'Customer address should be empty'];
     }
 }
