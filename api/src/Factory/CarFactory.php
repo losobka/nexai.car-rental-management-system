5d4db49ef687b7e46684eee90c6fd3375afe9016
losobka <lukasz.osobka@gmail.com>
2025-01-19 08:22:32 +0100

    #API - poprawki w App\Factory\CarFactory: - przeniesienie metody generateUniqueRegistrationNumber do custom faker providera (App\Service\Faker\Provider\Car::carRegistrationNumber()) - przeniesienie metody generateUniqueVin do custom faker providera ( (App\Service\Faker\Provider\Car::vin()) - usunięcie metody generateGeoCoordinate - użycie providerów z latitude i longitude z fakera - konfiguracja serwisu app.faker (locale: pl_PL, addProvider: App\Service\Faker\Provider\Car) - konfiguracja zenstruck_foundry.faker.service

--- api/src/Factory/CarFactory.php
+++ api/src/Factory/CarFactory.php
@@ -3,9 +3,6 @@
 namespace App\Factory;
 
 use App\Entity\Car;
-use App\Service\VinGenerator;
-use App\Service\RegistrationNumberGenerator;
-use Random\RandomException;
 use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 
 /**
@@ -13,21 +10,6 @@ use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
  */
 final class CarFactory extends PersistentProxyObjectFactory
 {
-    private RegistrationNumberGenerator $registrationNumberGenerator;
-    private VinGenerator $vinGenerator;
-    private array $generatedVins = [];
-    private array $generatedRegistrationNumbers = [];
-
-
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     */
-    public function __construct(RegistrationNumberGenerator $registrationNumberGenerator, VinGenerator $vinGenerator) {
-        parent::__construct();
-        $this->registrationNumberGenerator = $registrationNumberGenerator;
-        $this->vinGenerator = $vinGenerator;
-    }
-
     public static function class(): string
     {
         return Car::class;
@@ -40,11 +22,11 @@ final class CarFactory extends PersistentProxyObjectFactory
     {
         $defaults = [
             'brand' => self::faker()->randomElement(Car::AVAILABLE_BRANDS),
-            'currentLat' => $this->generateGeoCoordinate(),
-            'currentLng' => $this->generateGeoCoordinate(),
-            'registrationNumber' => $this->generateUniqueRegistrationNumber(),
-            'rented' => self::faker()->boolean(),
-            'vin' => $this->generateUniqueVin(),
+            'currentLat' => self::faker()->latitude(),
+            'currentLng' => self::faker()->longitude(),
+            'registrationNumber' => self::faker()->carRegistrationNumber(),
+            'rented' => self::faker()->randomElement([true, false]),
+            'vin' => self::faker()->vin(),
         ];
 
         if ($defaults['rented']) {
@@ -63,40 +45,5 @@ final class CarFactory extends PersistentProxyObjectFactory
         return $this
             // ->afterInstantiate(function(Car $car): void {})
         ;
-    }
-
-    private function generateUniqueVin(): string
-    {
-        do {
-            try {
-                $vin = $this->vinGenerator->generate();
-            } catch (RandomException) {
-                unset($vin);
-            }
-        } while (isset($vin) && is_int(array_search($vin, $this->generatedVins, true)));
-
-        return $vin;
-    }
-
-    private function generateUniqueRegistrationNumber(): string
-    {
-        do {
-            try {
-                $registrationNumber = $this->registrationNumberGenerator->generate(
-                    (bool) rand(0, 100)
-                        ? null
-                        : self::faker()->randomElement(RegistrationNumberGenerator::COMMON_CITY_PREFIXES)
-                );
-            } catch (RandomException) {
-                unset($registrationNumber);
-            }
-        } while (isset($registrationNumber) && is_int(array_search($registrationNumber, $this->generatedRegistrationNumbers, true)));
-
-        return $registrationNumber;
-    }
-
-    private function generateGeoCoordinate(): float
-    {
-        return self::faker()->randomFloat(5, -90, 90);
     }
 }
