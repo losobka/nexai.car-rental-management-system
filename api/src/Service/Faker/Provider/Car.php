e21b3efa5ef0f7d6322026fa8f79f010c94fc08b
losobka <lukasz.osobka@gmail.com>
2025-01-19 08:22:32 +0100

    @API - poprawki w App\Factory\CarFactory: - przeniesienie metody generateUniqueRegistrationNumber do custom faker providera (App\Service\Faker\Provider\Car::carRegistrationNumber()) - przeniesienie metody generateUniqueVin do custom faker providera ( (App\Service\Faker\Provider\Car::vin()) - usunięcie metody generateGeoCoordinate - użycie providerów z latitude i longitude z fakera - konfiguracja serwisu app.faker (locale: pl_PL, addProvider: App\Service\Faker\Provider\Car) - konfiguracja zenstruck_foundry.faker.service

---
+++ api/src/Service/Faker/Provider/Car.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Service\Faker\Provider;
+
+use App\Service\RegistrationNumberGenerator;
+use App\Service\VinGenerator;
+use Faker\Generator;
+use Faker\Provider\Base;
+use Random\RandomException;
+
+class Car extends Base
+{
+    public static VinGenerator $vinGenerator;
+    public static RegistrationNumberGenerator $registrationNumberGenerator;
+    public static array $generatedRegistrationNumbers = [];
+    public static array $generatedVins = [];
+
+    public function __construct(Generator $generator, VinGenerator $vinGenerator, RegistrationNumberGenerator $registrationNumberGenerator)
+    {
+        parent::__construct($generator);
+        self::$vinGenerator = $vinGenerator;
+        self::$registrationNumberGenerator = $registrationNumberGenerator;
+    }
+
+    public static function vin(): string
+    {
+        do {
+            try {
+                $vin = self::$vinGenerator->generate();
+            } catch (RandomException) {
+                unset($vin);
+            }
+        } while (isset($vin) && is_int(array_search($vin, self::$generatedVins, true)));
+
+        return $vin;
+    }
+
+    public static function carRegistrationNumber(): string
+    {
+        do {
+            try {
+                $registrationNumber = self::$registrationNumberGenerator->generate(
+                    (bool) rand(0, 100)
+                        ? null
+                        : self::randomElement(RegistrationNumberGenerator::COMMON_CITY_PREFIXES)
+                );
+            } catch (RandomException) {
+                unset($registrationNumber);
+            }
+        } while (isset($registrationNumber) && is_int(array_search($registrationNumber, self::$generatedRegistrationNumbers, true)));
+
+        return $registrationNumber;
+    }
+}
