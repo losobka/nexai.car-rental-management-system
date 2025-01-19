5ac02c9585237aec6f609a637506d8e7ffb40326
losobka <lukasz.osobka@gmail.com>
2025-01-19 06:24:54 +0100

    #API - stworzenie App\Service\VinGenerator (na potrzeby fixtur App\Entity\Car::vin) + test jednostkowy

---
+++ api/src/Service/VinGenerator.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Random\RandomException;
+
+class VinGenerator
+{
+    public const LENGTH = 17;
+
+    /**
+     * @throws RandomException
+     */
+    public function generate(): string
+    {
+        return mb_strtoupper(chr(random_int(65, 90)) . mb_substr(md5(random_bytes(1024)), 0, self::LENGTH - 1));
+    }
+}
