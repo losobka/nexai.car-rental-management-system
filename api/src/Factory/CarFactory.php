ce94096d0784e65f2fdbd25d64825750f80f649f
losobka <lukasz.osobka@gmail.com>
2025-01-19 06:40:56 +0100

    #API - poprawki w App\Factory\CarFactory: - generowanie unikalnego numeru rejestracyjnego (App\Entity\ Car::registrationNumber) - generowanie unikalnego numeru vin (App\Entity\ Car::vin) - generowanie adresu klienta (App\Entity\Car::customerAddress) wg. formatu "ul. Ulica, 00-000 Miasto"

--- api/src/Factory/CarFactory.php
+++ api/src/Factory/CarFactory.php
@@ -3,6 +3,9 @@
 namespace App\Factory;
 
 use App\Entity\Car;
+use App\Service\VinGenerator;
+use App\Service\RegistrationNumberGenerator;
+use Random\RandomException;
 use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 
 /**
@@ -10,13 +13,19 @@ use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
  */
 final class CarFactory extends PersistentProxyObjectFactory
 {
+    private RegistrationNumberGenerator $registrationNumberGenerator;
+    private VinGenerator $vinGenerator;
+    private array $generatedVins = [];
+    private array $generatedRegistrationNumbers = [];
+
+
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
      */
-    public function __construct()
-    {
+    public function __construct(RegistrationNumberGenerator $registrationNumberGenerator, VinGenerator $vinGenerator) {
+        parent::__construct();
+        $this->registrationNumberGenerator = $registrationNumberGenerator;
+        $this->vinGenerator = $vinGenerator;
     }
 
     public static function class(): string
@@ -26,23 +35,21 @@ final class CarFactory extends PersistentProxyObjectFactory
 
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
      */
     protected function defaults(): array|callable
     {
         $defaults = [
-            'brand' => Car::AVAILABLE_BRANDS[array_rand(Car::AVAILABLE_BRANDS)],
-            'currentLat' => self::faker()->randomFloat(5, -90, 90),
-            'currentLng' => self::faker()->randomFloat(5, -90, 90),
-            'registrationNumber' => mb_strtoupper(self::faker()->text(12)),
+            'brand' => self::faker()->randomElement(Car::AVAILABLE_BRANDS),
+            'currentLat' => $this->generateGeoCoordinate(),
+            'currentLng' => $this->generateGeoCoordinate(),
+            'registrationNumber' => $this->generateUniqueRegistrationNumber(),
             'rented' => self::faker()->boolean(),
-            'vin' => mb_strtoupper(self::faker()->text(17)),
+            'vin' => $this->generateUniqueVin(),
         ];
 
         if ($defaults['rented']) {
             $defaults['customerEmail'] = mb_strtolower(self::faker()->email());
-            $defaults['customerAddress'] = mb_strtoupper(self::faker()->address());
+            $defaults['customerAddress'] = 'ul. ' . self::faker()->address();
         }
 
         return $defaults;
@@ -56,5 +63,40 @@ final class CarFactory extends PersistentProxyObjectFactory
         return $this
             // ->afterInstantiate(function(Car $car): void {})
         ;
+    }
+
+    private function generateUniqueVin(): string
+    {
+        do {
+            try {
+                $vin = $this->vinGenerator->generate();
+            } catch (RandomException) {
+                unset($vin);
+            }
+        } while (isset($vin) && is_int(array_search($vin, $this->generatedVins, true)));
+
+        return $vin;
+    }
+
+    private function generateUniqueRegistrationNumber(): string
+    {
+        do {
+            try {
+                $registrationNumber = $this->registrationNumberGenerator->generate(
+                    (bool) rand(0, 100)
+                        ? null
+                        : self::faker()->randomElement(RegistrationNumberGenerator::COMMON_CITY_PREFIXES)
+                );
+            } catch (RandomException) {
+                unset($registrationNumber);
+            }
+        } while (isset($registrationNumber) && is_int(array_search($registrationNumber, $this->generatedRegistrationNumbers, true)));
+
+        return $registrationNumber;
+    }
+
+    private function generateGeoCoordinate(): float
+    {
+        return self::faker()->randomFloat(5, -90, 90);
     }
 }
