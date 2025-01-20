247391c8e99f799c516b0a181aa292b1bf1d1dc8
losobka <lukasz.osobka@gmail.com>
2025-01-20 14:22:37 +0100

    #DOCKER php-fpm poprawka entrypointu

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 ATTEMPTS_LEFT_TO_REACH_DATABASE=60
 
-until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
-    if [ $? -eq 0 ]; then
+until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || php bin/console dbal:run-sql -q "SELECT 1" 2>&1; do
+    if [ "$?" -eq 0 ]; then
         ATTEMPTS_LEFT_TO_REACH_DATABASE=0
         break
     fi
@@ -16,9 +16,9 @@ until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/con
     echo "[NOTICE] Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
 done
 
-[ $DROP_DATABASE == true ] && bin/console doctrine:database:drop --if-exists --force
-[ $CREATE_DATABASE == true ] && bin/console doctrine:database:create
-[ $CREATE_SCHEMA == true ] && bin/console doctrine:schema:create
-[ $LOAD_FIXTURES == true ] && bin/console doctrine:fixtures:load --no-interaction --append
+[ "$DROP_DATABASE" ] && bin/console doctrine:database:drop --if-exists --force
+[ "$CREATE_DATABASE" ] && bin/console doctrine:database:create
+[ "$CREATE_SCHEMA" ] && bin/console doctrine:schema:create
+[ "$LOAD_FIXTURES" ] && bin/console doctrine:fixtures:load --no-interaction --append
 
 php-fpm -R
