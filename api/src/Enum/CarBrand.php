1482b3758b8d6b7d2a84c8cee674c9ed0c24e9cd
losobka <lukasz.osobka@gmail.com>
2025-01-21 21:46:23 +0100

    @API refaktor: - wydzielenie App\Entity\Car::AVAILABLE_BRANDS do App\Enum\CarBrand

---
+++ api/src/Enum/CarBrand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Enum;
+
+enum CarBrand: string
+{
+    case AUDI = 'Audi';
+    case BMW ='BMW';
+    case CHEVROLET = 'Chevrolet';
+    case FERRARI = 'Ferrari';
+    case FORD = 'Ford';
+    case HONDA = 'Honda';
+    case HYUNDAI = 'Hyundai';
+    case JEEP = 'Jeep';
+    case KEI = 'Kia';
+    case LAMBORGHINI = 'Lamborghini';
+    case LEXUS = 'Lexus';
+    case MAZDA = 'Mazda';
+    case MERCEDES_BENZ = 'Mercedes-Benz';
+    case NISSAN = 'Nissan';
+    case PORSCHE = 'Porsche';
+    case SUBARU = 'Subaru';
+    case TESLA = 'Tesla';
+    case TOYOTA = 'Toyota';
+    case VOLKSWAGEN = 'Volkswagen';
+    case VOLVO ='Volvo';
+
+    public static function all(): array
+    {
+        return [];
+    }
+}
